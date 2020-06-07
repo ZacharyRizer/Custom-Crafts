2074c2da011086bcbdb987303395033c284b2b5e
William Schrader <wfschrad@gmail.com>
2020-06-06 22:32:07 -0700

    Add Apollo client.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import ApolloTest from './components/ApolloTest'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 function App() {
-  return <h1>Custom Crafts</h1>;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <h1>Custom Crafts</h1>
+        <h2>This rendered</h2>
+        <Route path='/apollo-test' component={ApolloTest} />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
