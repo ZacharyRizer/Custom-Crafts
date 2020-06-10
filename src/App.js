5307d09a7fa2b20a56f627d7dd5178c2c271ab39
Ian Magenta <ianmagenta@gmail.com>
2020-06-09 21:08:12 -0700

    Fixed up nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { ContextProvider } from './Context';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import ShipPage from './components/ShipPage';
-import NavBar from './components/NavBar';
-import Splash from './components/Splash';
-import Shop from './components/Shop';
-import PrivateRoute from './components/PrivateRoute';
-import Profile from './components/Profile';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { ContextProvider } from "./Context";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import ShipPage from "./components/ShipPage";
+import NavBar from "./components/NavBar";
+import Splash from "./components/Splash";
+import Shop from "./components/Shop";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./components/Profile";
 
 const App = () => {
   const client = new ApolloClient({ uri: "http://localhost:5000/graphql" });
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,69 @@
-import React, {useContext} from 'react';
-import {Context} from '../Context'
-import { useAuth0 } from '../react-auth0-spa';
-import { Link } from 'react-router-dom';
-import { Frame, Heading, Button } from "arwes";
-import {TextField} from '@material-ui/core';
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import { useAuth0 } from "../react-auth0-spa";
+import { Frame, Heading, Button, Link, Appear } from "arwes";
 
+function keyChecker(e) {
+  if (e.key === "Enter") {
+    window.location.href = `/search/${e.target.value}`;
+  }
+}
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-  const {numItems} = useContext(Context)
+  const { numItems } = useContext(Context);
 
   return (
-    <Frame animate level={1} corners={6} layer='primary'>
-      <div className='navbar'>
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          <Heading style={{ margin: "10px", padding: "15px", fontSize: "32px" }} node='h1'>Custom Crafts</Heading>
-        </Link>
-        <TextField color='green' placeholder='Search For Ships!' />
-        {!isAuthenticated && (
-        <div className='navbar_buttons'>
-          <Button onClick={() => loginWithRedirect({})} animate layer='success' >Login</Button>
-          <Link to="/cart">
-            <Button animate disabled >Cart: {numItems}</Button>
+    <Frame animate level={1} corners={6} layer="primary" style={{ marginBottom: 20 }}>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <div style={{ margin: "10px", padding: "15px" }}>
+          <Link href="/" style={{ textDecoration: "none" }}>
+            <Heading style={{ fontSize: "32px", margin: 0 }} node="h1">
+              Custom Crafts
+            </Heading>
           </Link>
         </div>
-        )}
-        {isAuthenticated && (
-        <div className='navbar_buttons'>
-          <Link to="/profile">
-            <Button animate layer='success'>Profile</Button>
-          </Link>
-          <Link to="/cart">
-            <Button animate disabled >Cart: {numItems}</Button>
-          </Link>
-          <Button onClick={() => logout()} animate layer='alert'>Log-out</Button>
+        <Frame buttonProps={{ style: { cursor: "text" } }} corners={0}>
+          <Appear>
+            <input
+              style={{ padding: 10, backgroundColor: "transparent", outline: "none", border: "none", color: "#26dafd" }}
+              type="text"
+              placeholder="Search for Ships"
+              onKeyDown={keyChecker}
+              backgroundImage={'element(<i className="mdi mdi-magnify" style={{ paddingLeft: 8, cursor: "text" }} />)'}
+            ></input>
+          </Appear>
+        </Frame>
+        <div style={{ flexGrow: 1, display: "flex", justifyContent: "flex-end" }}>
+          {!isAuthenticated ? (
+            <>
+              <Button style={{ marginRight: 25 }} onClick={() => loginWithRedirect({})} animate layer="secondary">
+                Login
+              </Button>
+              <Link href="/cart">
+                <Button style={{ marginRight: 25 }} animate>
+                  Cart: {numItems}
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/profile">
+                <Button animate style={{ marginRight: 25 }} layer="secondary">
+                  Profile
+                </Button>
+              </Link>
+              <Link href="/cart">
+                <Button animate style={{ marginRight: 25 }}>
+                  Cart: {numItems}
+                </Button>
+              </Link>
+              <Button animate layer="alert" style={{ marginRight: 25 }} onClick={() => logout()}>
+                Log-out
+              </Button>
+            </>
+          )}
         </div>
-        )}
       </div>
     </Frame>
   );
