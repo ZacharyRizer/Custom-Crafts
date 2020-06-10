8e93785deaf30337452ab2dea40d32aa4d6312b3
Ian Magenta <ianmagenta@gmail.com>
2020-06-09 22:04:42 -0700

    Adjusted Search Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import { Frame, Heading, Button, Link, Appear } from "arwes";
 
 function keyChecker(e) {
   if (e.key === "Enter") {
-    window.location.href = `/search/${e.target.value}`;
+    window.location.href = `/shop`;
   }
 }
 
@@ -16,14 +16,14 @@ const NavBar = () => {
   return (
     <Frame animate level={1} corners={6} layer="primary" style={{ marginBottom: 20 }}>
       <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-        <div style={{ margin: "10px", padding: "15px" }}>
+        <div style={{ margin: "10px", padding: "15px", flexGrow: 1 }}>
           <Link href="/" style={{ textDecoration: "none" }}>
             <Heading style={{ fontSize: "32px", margin: 0 }} node="h1">
               Custom Crafts
             </Heading>
           </Link>
         </div>
-        <Frame buttonProps={{ style: { cursor: "text" } }} corners={0}>
+        <Frame style={{ flexGrow: 1 }} buttonProps={{ style: { cursor: "text" } }} corners={0}>
           <Appear style={{ display: "flex", alignItems: "center" }}>
             <input
               style={{
@@ -35,6 +35,7 @@ const NavBar = () => {
                 border: "none",
                 color: "#26dafd",
                 zIndex: 1,
+                width: "100%",
               }}
               type="text"
               placeholder="Search for Ships"
@@ -51,8 +52,7 @@ const NavBar = () => {
               </Button>
               <Link href="/cart">
                 <Button style={{ marginRight: 25 }} animate>
-                  <i class="mdi mdi-cart" />
-                  Cart: {numItems}
+                  <i class="mdi mdi-cart" />: {numItems}
                 </Button>
               </Link>
             </>
@@ -65,8 +65,8 @@ const NavBar = () => {
               </Link>
               <Link href="/cart">
                 <Button animate style={{ marginRight: 25 }}>
-                  <i class="mdi mdi-cart" />
-                  Cart: {numItems}
+                  <i class="mdi mdi-cart"> </i>
+                  {numItems}
                 </Button>
               </Link>
               <Button animate layer="alert" style={{ marginRight: 25 }} onClick={() => logout()}>
