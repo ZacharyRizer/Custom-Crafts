ad0c4f96aad4231dd721b4de3fd3c486578e2c43
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-10 12:47:20 -0700

    fix merge bugs

--- src/components/ShipCard.js
+++ src/components/ShipCard.js
@@ -70,18 +70,6 @@ const ShipCard = ({ ship }) => {
             Limited Stock
           </Button>
         ) : (
-<<<<<<< HEAD
-              <Button
-                corners={0}
-                style={{ pointerEvents: 'none' }}
-                layer="disabled">
-                Out of Stock
-              </Button>
-            )}
-        <Button onClick={() => (window.location.href = '/')} layer="secondary">
-          View Details
-        </Button>
-=======
           <Button
             corners={0}
             style={{ pointerEvents: 'none' }}
@@ -92,7 +80,6 @@ const ShipCard = ({ ship }) => {
         <Link to={`/ships/${id}`}>
           <Button layer="secondary">View Details</Button>
         </Link>
->>>>>>> 54745b2b01ec0011cfec13db60ff35097c6ce4ea
       </div>
     </Frame>
   );
