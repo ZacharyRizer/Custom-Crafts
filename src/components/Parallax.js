df4f374703a4638ab9010dbcf8b8c98e13772335
Abdullah Wafy <red.sulphur786@gmail.com>
2020-06-09 20:56:15 -0700

    separate parallax and carousel react components, cart icon for shopping cart button

---
+++ src/components/Parallax.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Paragraph } from 'arwes';
+
+const Parallax = () => (
+<>
+    <div>
+        <div>
+            {/* <Image className="homeIMG" animate resources='https://cdn.suwalls.com/wallpapers/fantasy/giant-spaceship-passing-the-planet-53912-1920x1200.jpg' /> */}
+            <Paragraph className="plax">
+                Welcome to Cruising Crafts! Check out our great Summer Sale! Prices
+                half off all SpaceShips!
+            </Paragraph>
+        </div>
+        <div className="parallax"></div>
+        <div>
+            {/* <Image className="homeIMG" animate resources='https://www.sciencealert.com/images/2019-02/processed/GettyImages-861460116_1_1024.jpg' /> */}
+            <Paragraph className="plax">
+                We've got Luxury spacecrafts, Combat Shuttles, and Performance Racers
+                for those with a little fast and furious in their genes!
+            </Paragraph>
+        </div>
+        <div className="parallaxTwo"></div>
+        <div>
+        <div>
+                {/* <Image className="homeIMG" animate src='https://cdn.suwalls.com/wallpapers/fantasy/giant-spaceship-passing-the-planet-53912-1920x1200.jpg' /> */}
+            <Paragraph className="plax">
+                    Welcome to Cruising Crafts! Check out our great Summer Sale! Prices
+                    half off all SpaceShips!
+            </Paragraph>
+        </div>
+        <div className="parallaxThree"></div>
+        <div>
+                {/* <Image className="homeIMG" animate src='https://www.sciencealert.com/images/2019-02/processed/GettyImages-861460116_1_1024.jpg' /> */}
+            <Paragraph className="plax">
+                    We've got Luxury spacecrafts, Combat Shuttles, and Performance
+                    Racers for those with a little fast and furious in their genes!
+            </Paragraph>
+        </div>
+        <div className="parallaxFour"></div>
+        <div>
+                {/* <Image className="homeIMG" animate src='https://wallpaperaccess.com/full/500417.jpg' /> */}
+            <Paragraph className="plax">
+                    The best deals in the Universe ranging from $599,999 to $999,999
+            </Paragraph>
+        </div>
+        <div className="parallaxFive"></div>
+        <div>
+                    {/* <Image className="homeIMG" animate resources='https://cdn.suwalls.com/wallpapers/fantasy/giant-spaceship-passing-the-planet-53912-1920x1200.jpg' /> */}
+            <Paragraph className="plax">
+                        Welcome to Cruising Crafts! Check out our great Summer Sale! Prices
+                        half off all SpaceShips!
+            </Paragraph>
+        </div>
+        <div className="parallaxSix"></div>
+        </div>
+    </div>
+</>
+);
+
+export default Parallax;
