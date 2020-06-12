397ceebafa8884e2153758e2eeb32b6bf01d9f47
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-11 17:18:12 -0700

    small bugs

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect, Fragment } from "react";
-import { useAuth0 } from "../react-auth0-spa";
-import { Link } from "react-router-dom";
-import Axios from "axios";
-import { Frame, Heading, Line, Table, Button } from "arwes";
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import { Frame, Heading, Line, Table, Button } from 'arwes';
 
 const Profile = () => {
   let [orders, setOrders] = useState([]);
 
   // const id = props.match.params.customerId;
-  const user = JSON.parse(localStorage.getItem("custom_crafts_userObj"));
-  console.log("id 22", user.id);
+  const user = JSON.parse(localStorage.getItem('custom_crafts_userObj'));
   const query = `
   {
     customer(customerId: ${user.id}) {
@@ -29,8 +27,8 @@ const Profile = () => {
   useEffect(() => {
     (async () => {
       const res = await Axios({
-        url: "http://localhost:5000/graphql",
-        method: "post",
+        url: 'http://localhost:5000/graphql',
+        method: 'post',
         data: {
           query,
         },
@@ -55,43 +53,39 @@ const Profile = () => {
     <Fragment>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <Frame
           style={{
-            border: "2px dotted",
-          }}
-        >
+            border: '2px dotted',
+          }}>
           <img src={user.picture} alt="Profile" />
         </Frame>
       </div>
-      <h3 style={{ textAlign: "center" }}>{user.name}</h3>
-      <p style={{ textAlign: "center", fontSize: 12 }}>{user.email}</p>
-      <Heading node="h1" style={{ textAlign: "center" }}>
+      <h3 style={{ textAlign: 'center' }}>{user.name}</h3>
+      <p style={{ textAlign: 'center', fontSize: 12 }}>{user.email}</p>
+      <Heading node="h1" style={{ textAlign: 'center' }}>
         Purchase History
       </Heading>
       <Line animate layer="success" />
       <Table
-        headers={["Order Number", "Ordered Items", "Quantity"]}
+        headers={['Order Number', 'Ordered Items', 'Quantity']}
         dataset={[entries]}
       />
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <Link to="/shop">
           <Button animate layer="secondary">
             Back to Shop <i className="mdi mdi-rocket-launch" />
           </Button>
         </Link>
       </div>
-      {/* <code>{JSON.stringify(user, null, 2)}</code> */}
     </Fragment>
   );
 };
