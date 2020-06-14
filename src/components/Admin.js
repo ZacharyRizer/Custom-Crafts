1ec15399e4235026d1e618d55fffa98afe640510
William Schrader <wfschrad@gmail.com>
2020-06-14 02:00:18 -0700

    Retrieve inventory data for admin component.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -18,7 +18,104 @@ import {
 } from 'arwes';
 
 const Admin = () => {
-  return <Frame>Checkout all these sweet admin things!</Frame>;
+  const { getTokenSilently } = useAuth0();
+  const token = getTokenSilently();
+  let [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+    const is = `
+      {
+        ships{
+          edges{
+            node{
+              id
+              stock
+              name
+              category {
+                name
+              }
+              manufacturer {
+                name
+              }
+              orderItems{
+                quantity
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    (async () => {
+      const inventoryRes = await Axios({
+        url: apiBaseUrl,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'post',
+        data: {
+          query: is,
+        },
+      });
+      const data = inventoryRes.data.data.ships.edges;
+      const inventory = data.map((item) => {
+        return item.node;
+      });
+      setInventory(inventory);
+      console.log('inventory', inventory);
+    })();
+  }, []);
+  let entries;
+  if (inventory) {
+    entries = inventory.map((item => {
+      return [
+        <>
+          <span>{item.id}</span>,
+          <span>{item.name}</span>,
+          <span>{item.stock}</span>,
+          <span>{item.id}</span>,
+          <span>{item.category.name}</span>,
+          <span>{item.manufacturer.name}</span>
+        </>
+      ]
+    }))
+  }
+
+  return (
+    <>{entries ? (
+      <>
+        <Frame>Checkout all these sweet admin things!</Frame>
+        <Frame
+          layer={'primary'}
+          animate
+          level={0}
+          corners={4}
+          style={{ margin: '10px 30px 0 30px' }}>
+          <Table
+            style={{ padding: 20, marginBottom: 0 }}
+            animate
+            headers={[
+              'Stock ID',
+              'Product Name',
+              'Stock Quantity',
+              'Total Sold',
+              'Product Category',
+              'Manufacturer'
+            ]}
+            dataset={entries}
+          />
+
+          {/* <Content style={{ paddingLeft: 20 }}>
+          <h1>
+            Put data here
+          </h1>
+        </Content> */}
+        </Frame>
+      </>
+    ) : null}
+    </>
+
+  )
 };
 
 export default Admin;
--- src/components/AllReviews.js
+++ src/components/AllReviews.js
@@ -5,49 +5,53 @@ import { Frame, Content, Image, Line } from "arwes";
 
 // component to create new review
 const AllReviews = (props) => {
-  const reviews = props.reviews;
-  console.log("this is the review object:", reviews);
+    const reviews = props.reviews;
+    console.log("this is the review object:", reviews);
 
-  return (
-    <Frame
-      layer={"primary"}
-      animate
-      level={0}
-      corners={0}
-      style={{ height: "100%", marginBottom: 20 }}
-      children={{ style: { color: "red" } }}
-    >
-      <div style={{ height: 388, maxHeight: 388, padding: 20, overflowY: "scroll" }}>
-        {reviews &&
-          reviews.map((review) => (
-            <Content>
-              <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
-                {review ? (
-                  <Image resources={review.customer.picture} style={{ width: 32, height: 32 }} />
-                ) : (
-                  <Image resources="/android-chrome-192x192.png" style={{ width: 32, height: 32 }} />
-                )}
+    return (
+        <Frame
+            layer={"primary"}
+            animate
+            level={0}
+            corners={0}
+            style={{ height: "100%", marginBottom: 20 }}
+            children={{ style: { color: "red" } }}
+        >
+            <div style={{ height: 388, maxHeight: 388, padding: 20, overflowY: "scroll" }}>
+                {reviews &&
+                    reviews.map((review) => (
+                        <Content>
+                            <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+                                {(review && review.customer) ? (
+                                    <>
+                                        <Image resources={review.customer.picture} style={{ width: 32, height: 32 }} />
+                                        <blockquote>{review.customer.name}</blockquote>
+                                    </>
+                                ) : (<>
+                                    <Image resources="/android-chrome-192x192.png" style={{ width: 32, height: 32 }} />
 
-                <blockquote>{review.customer.name}</blockquote>
-              </div>
-              <div>
-                <i className="mdi mdi-star" />
-                <i className="mdi mdi-star" />
-                <i className="mdi mdi-star" />
-                <i className="mdi mdi-star" />
-                <i className="mdi mdi-star" />
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Donec et odio pellentesque diam volutpat. Ac tincidunt vitae semper quis lectus
-                nulla at volutpat diam.
+
+                                </>
+                                    )}
+                            </div>
+                            <div>
+                                <i className="mdi mdi-star" />
+                                <i className="mdi mdi-star" />
+                                <i className="mdi mdi-star" />
+                                <i className="mdi mdi-star" />
+                                <i className="mdi mdi-star" />
+                            </div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                dolore magna aliqua. Donec et odio pellentesque diam volutpat. Ac tincidunt vitae semper quis lectus
+                                nulla at volutpat diam.
               </p>
-              <Line animate />
-            </Content>
-          ))}
-      </div>
-    </Frame>
-  );
+                            <Line animate />
+                        </Content>
+                    ))}
+            </div>
+        </Frame>
+    );
 };
 
 export default AllReviews;
