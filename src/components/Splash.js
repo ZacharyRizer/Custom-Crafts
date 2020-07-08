f64429183781fa411c4956181b2fc9c078ba5354
Ian Magenta <ianmagenta@gmail.com>
2020-07-07 17:13:43 -0700

    added some styling to home footer

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Row,
-  Col,
-  Frame,
-  Button,
-  Footer,
-  Line,
-  Content,
-  Words,
-  Heading,
-} from 'arwes';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Frame, Button, Footer, Line, Content, Words, Heading } from "arwes";
 
 const Splash = () => {
   const [shipNumber, setShipNumber] = useState(1);
-  const [shipColor, setShipColor] = useState('blue');
+  const [shipColor, setShipColor] = useState("blue");
 
   useEffect(() => {
     let i = 1;
     let j = 0;
-    const color = ['blue', 'green', 'orange', 'red'];
+    const color = ["blue", "green", "orange", "red"];
     const numberOfShips = 20;
     const interval = setInterval(() => {
       i++;
@@ -43,25 +33,27 @@ const Splash = () => {
 
   return (
     <>
-      <Row style={{ padding: '0 20px 20px 20px' }}>
+      <Row style={{ padding: "0 20px 20px 20px" }}>
         <Col s={12} m={6}>
           <Content>
             <h1
               style={{
                 fontSize: 100,
                 margin: 0,
-                textAlign: 'center',
-                verticalAlign: 'middle',
-              }}>
+                textAlign: "center",
+                verticalAlign: "middle",
+              }}
+            >
               The Galaxy's
             </h1>
             <h1
               style={{
                 fontSize: 100,
                 margin: 0,
-                textAlign: 'center',
-                verticalAlign: 'middle',
-              }}>
+                textAlign: "center",
+                verticalAlign: "middle",
+              }}
+            >
               Finest Ships
             </h1>
           </Content>
@@ -69,93 +61,80 @@ const Splash = () => {
           <Content>
             <Row>
               <Col s={12} m={6}>
-                <Frame
-                  layer={'primary'}
-                  animate
-                  level={0}
-                  corners={2}
-                  style={{ marginBottom: 20 }}>
+                <Frame layer={"primary"} animate level={0} corners={2} style={{ marginBottom: 20 }}>
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     Prices set to Stun,
                   </p>
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     Not Kill!
                   </p>
                 </Frame>
               </Col>
               <Col s={12} m={6}>
-                <Frame
-                  layer={'primary'}
-                  animate
-                  level={0}
-                  corners={2}
-                  style={{ marginBottom: 20 }}>
+                <Frame layer={"primary"} animate level={0} corners={2} style={{ marginBottom: 20 }}>
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     Best Deals
                   </p>
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     This Side of the Nebula!
                   </p>
                 </Frame>
               </Col>
               <Col s={12} m={6}>
-                <Frame
-                  layer={'primary'}
-                  animate
-                  level={0}
-                  corners={2}
-                  style={{ marginBottom: 0 }}>
+                <Frame layer={"primary"} animate level={0} corners={2} style={{ marginBottom: 0 }}>
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     "Glaxnar's Favorite!"
                   </p>
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     - Glaxnar The Devourer
                   </p>
                 </Frame>
               </Col>
               <Col s={12} m={6}>
-                <Frame
-                  layer={'primary'}
-                  animate
-                  level={0}
-                  corners={2}
-                  style={{ marginBottom: 0 }}>
+                <Frame layer={"primary"} animate level={0} corners={2} style={{ marginBottom: 0 }}>
                   <div
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     "
                     <i className="mdi mdi-star" />
                     <i className="mdi mdi-star" />
@@ -166,9 +145,10 @@ const Splash = () => {
                   <p
                     style={{
                       margin: 0,
-                      textAlign: 'center',
-                      fontStyle: 'italic',
-                    }}>
+                      textAlign: "center",
+                      fontStyle: "italic",
+                    }}
+                  >
                     - Spaceships Monthly
                   </p>
                 </Frame>
@@ -178,48 +158,41 @@ const Splash = () => {
           <Line animate />
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
               paddingTop: 90,
-            }}>
+            }}
+          >
             <Link to="/shop">
-              <Button corners={4} layer={'secondary'} animate>
+              <Button corners={4} layer={"secondary"} animate>
                 <div
                   style={{
                     padding: 20,
-                    display: 'flex',
-                    alignItems: 'center',
-                  }}>
-                  <Heading
-                    style={{ margin: 0, fontSize: 50, textShadow: 'none' }}>
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <Heading style={{ margin: 0, fontSize: 50, textShadow: "none" }}>
                     <Words layer="secondary">Let's Fly</Words>
                   </Heading>
-                  <i
-                    style={{ fontSize: 75, paddingLeft: 20 }}
-                    class="mdi mdi-transfer-right"
-                  />
+                  <i style={{ fontSize: 75, paddingLeft: 20 }} class="mdi mdi-transfer-right" />
                 </div>
               </Button>
             </Link>
           </div>
         </Col>
         <Col s={12} m={6}>
-          <Frame
-            layer={'primary'}
-            animate
-            level={0}
-            corners={4}
-            style={{ marginBottom: 20 }}>
+          <Frame layer={"primary"} animate level={0} corners={4} style={{ marginBottom: 20 }}>
             <model-viewer
               style={{
-                backgroundColor: 'transparent',
-                height: '800px',
-                width: '100%',
-                margin: 'auto',
-                '--poster-color': 'transparent',
-                '--progress-bar-color': 'transparent',
-                '--progress-mask': 'transparent',
+                backgroundColor: "transparent",
+                height: "800px",
+                width: "100%",
+                margin: "auto",
+                "--poster-color": "transparent",
+                "--progress-bar-color": "transparent",
+                "--progress-mask": "transparent",
               }}
               src={`/spaceships/ship${shipNumber}_${shipColor}.glb`}
               alt="A 3D model of a spaceship"
@@ -227,96 +200,73 @@ const Splash = () => {
               auto-rotate
               camera-controls
               exposure={0.75}
-              interaction-prompt="none"></model-viewer>
+              interaction-prompt="none"
+            ></model-viewer>
           </Frame>
         </Col>
       </Row>
-      <Footer animate>
-        <Row>
-          <Col>
-            <p>Site Created By:</p>
-          </Col>
-          <Col>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <p>Abdullah Wafy</p>
-              <a href="https://github.com/Seraph9">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-github" />
-                </Button>
-              </a>
-              <a href="https://www.linkedin.com/in/abdullah-wafy/">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-linkedin" />
-                </Button>
-              </a>
-            </div>
-          </Col>
-          <Col>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <p>Ian Magenta</p>
-              <a href="https://github.com/ianmagenta">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-github" />
-                </Button>
-              </a>
-              <a href="https://www.linkedin.com/in/ianmagenta/">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-linkedin" />
-                </Button>
-              </a>
-            </div>
-          </Col>
-          <Col>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <p>William Schrader</p>
-              <a href="https://github.com/wfschrad">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-github" />
-                </Button>
-              </a>
-              <a href="https://www.linkedin.com/in/william-schrader-7ba014199/">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-linkedin" />
-                </Button>
-              </a>
-            </div>
-          </Col>
-          <Col>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <p>Zachary Rizer</p>
-              <a href="https://github.com/ZacharyRizer">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-github" />
-                </Button>
-              </a>
-              <a href="https://www.linkedin.com/in/zachary-rizer/">
-                <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
-                  <i className="mdi mdi-linkedin" />
-                </Button>
-              </a>
-            </div>
-          </Col>
-        </Row>
+      <Footer animate style={{ position: "fixed", left: 0, bottom: 0, width: "100%", zIndex: 100 }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <p>Site Created By:</p>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <p>Abdullah Wafy</p>
+            <a href="https://github.com/Seraph9">
+              <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-github" />
+              </Button>
+            </a>
+            <a href="https://www.linkedin.com/in/abdullah-wafy/">
+              <Button layer="secondary" style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-linkedin" />
+              </Button>
+            </a>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <p>Ian Magenta</p>
+            <a href="https://github.com/ianmagenta">
+              <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-github" />
+              </Button>
+            </a>
+            <a href="https://www.linkedin.com/in/ianmagenta/">
+              <Button layer="secondary" style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-linkedin" />
+              </Button>
+            </a>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <p>William Schrader</p>
+            <a href="https://github.com/wfschrad">
+              <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-github" />
+              </Button>
+            </a>
+            <a href="https://www.linkedin.com/in/william-schrader-7ba014199/">
+              <Button layer="secondary" style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-linkedin" />
+              </Button>
+            </a>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <p>Zachary Rizer</p>
+            <a href="https://github.com/ZacharyRizer">
+              <Button style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-github" />
+              </Button>
+            </a>
+            <a href="https://www.linkedin.com/in/zachary-rizer/">
+              <Button layer="secondary" style={{ marginRight: 10, marginLeft: 10 }} animate>
+                <i className="mdi mdi-linkedin" />
+              </Button>
+            </a>
+          </div>
+        </div>
       </Footer>
     </>
   );
